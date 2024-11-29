105136640c29f554885fcf20fef2ba13861dc79f
Marcelo Silva <marcelo.costa.b@gmail.com>
2024-11-29 19:30:54 -0300

    Update ProductController.java

--- src/main/java/com/example/dummyjson/controller/ProductController.java
+++ src/main/java/com/example/dummyjson/controller/ProductController.java
@@ -5,7 +5,7 @@ import com.example.dummyjson.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
