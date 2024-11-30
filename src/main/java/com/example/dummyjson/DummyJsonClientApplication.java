4de374fe1c6bdd501a22aad0f7949912422fd4a2
Marcelo Silva <marcelo.costa.b@gmail.com>
2024-11-30 13:59:09 -0300

    Update DummyJsonClientApplication.java

--- src/main/java/com/example/dummyjson/DummyJsonClientApplication.java
+++ src/main/java/com/example/dummyjson/DummyJsonClientApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DummyJsonClientApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DummyJsonClientApplication.class, args);
     }
